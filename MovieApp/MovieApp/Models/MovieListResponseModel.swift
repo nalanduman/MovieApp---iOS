50a21cb88bf9b74e466618501a2b01c1133c8d90
Nalan Duman <dumann16@itu.edu.tr>
2024-12-11 20:23:49 +0300

    movie models added

---
+++ MovieApp/MovieApp/Models/MovieListResponseModel.swift
@@ -0,0 +1,54 @@
+//
+//  MovieListResponseModel.swift
+//  MovieApp
+//
+//  Created by Nalan Duman on 11.12.2024.
+//
+
+import Foundation
+
+struct MovieListResponseModel: Codable {
+    let page: Int?
+    let results: [Result]?
+    let totalResults: Int?
+    let totalPages: Int?
+    
+    enum CodingKeys: String, CodingKey {
+        case page
+        case results
+        case totalResults = "total_results"
+        case totalPages = "total_pages"
+    }
+}
+
+struct Result: Codable {
+    let adult: Bool?
+    let backdropPath: String?
+    let genreIds: [Int]?
+    let originalLanguage: String?
+    let originalTitle: String?
+    let overview: String?
+    let popularity: Double?
+    let posterPath: String?
+    let releaseDate: String?
+    let title: String?
+    let video: Bool?
+    let voteAverage: Double?
+    let voteCount: Int?
+    
+    enum CodingKeys: String, CodingKey {
+        case adult
+        case backdropPath = "backdrop_path"
+        case genreIds = "genre_ids"
+        case originalLanguage = "original_language"
+        case originalTitle = "original_title"
+        case overview
+        case popularity
+        case posterPath = "poster_path"
+        case releaseDate = "release_date"
+        case title
+        case video
+        case voteAverage = "vote_average"
+        case voteCount = "vote_count"
+    }
+}
