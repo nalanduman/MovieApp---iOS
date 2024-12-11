1aa0bf9fffb717a4d5b6be04cb8ada9961fe5818
Nalan Duman <dumann16@itu.edu.tr>
2024-12-11 18:15:20 +0300

    project created

---
+++ MovieApp/MovieAppTests/MovieAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  MovieAppTests.swift
+//  MovieAppTests
+//
+//  Created by Nalan Duman on 11.12.2024.
+//
+
+import Testing
+@testable import MovieApp
+
+struct MovieAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
